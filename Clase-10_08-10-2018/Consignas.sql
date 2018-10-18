a1c901e0ca3be7325ba79840bd92d582ee8efe7e
Norman Ruiz <norman.ruiz@icloud.com>
2018-10-18 04:23:38 -0300

    se eliminaron rachivos cargados de mas o que no correespondian y se comenzo con el TP sugerido N6

---
+++ Clase-10_08-10-2018/Consignas.sql
@@ -0,0 +1,22 @@
+-- PRIMERA PARTE DE LA CLASE
+--------------------------------
+-- Por cada tipo de propiedad, la cantidad de vecinos distintos que poseen propiedades de dicho tipo.
+
+-- Por cada tipo de propiedad, la cantidad de vecinos que poseen propiedades de dicho tipo.
+
+-- Listar apellido y nombres de los vecinos que tengan m�s de una propiedad sin superficie construida (superficie construida igual a cero)
+
+-- Listar apellido y nombres de los vecinos que tengan alguna propiedad con superficie construida y 
+-- ninguna propiedad sin superficie construida
+
+-- Listar el vecino que disponga de la casa con mayor valor por m2.
+
+-- Para cada vecino, listar apellido y nombre y la antig�edad menor de sus propiedades. Si un vecino no tiene
+-- propiedades registradas, listar una antig�edad con valor NULL.
+
+-- A todos los vecinos cuya sumatoria de valuaci�n de todas sus propiedades sea mayor a $4.000.000 y
+-- posean m�s de una propiedad se le aplicar� un impuesto del 5% del valor de sus propiedades 
+-- (a excepci�n del valor de su propiedad m�s cara).
+-- Listar apellido y nombre de los vecinos que deban pagar el impuesto y cu�nto deber�n pagar.
+
+
