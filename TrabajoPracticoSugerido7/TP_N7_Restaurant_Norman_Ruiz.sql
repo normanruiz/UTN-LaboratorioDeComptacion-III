bfd8886ea70cc6576eaa7edcbd3a83ab6a819715
Norman Ruiz <norman.ruiz@icloud.com>
2018-10-28 20:57:07 -0300

    Se agregaron .odt de clase 11, 12, 13 y actividades de clase 11 y 12.

---
+++ TrabajoPracticoSugerido7/TP_N7_Restaurant_Norman_Ruiz.sql
@@ -0,0 +1,43 @@
+﻿/*
+Universisas Tecnologica Nacional
+General pacheco
+Laboratorio de Computacioon III
+Profesor: Angel Simon
+A�o 2018
+Segundo cuatrimestre
+Turno Noche
+Alumno Norman Ruiz
+Trabajo Practico N�7
+*/
+
+GO
+USE TP_SUGERIDO_7;
+
+-- A) - Realizar un procedimiento almacenado que permita conocer el listado de ingredientes y sus 
+--      respectivas cantidades y unidades de medida. Se deberá enviar el código de receta. 
+
+
+
+-- B) - Realizar un procedimiento almacenado que permita conocer a partir de un código de 
+--      producto los datos de los proveedores que lo venden. Ordenado de menor a mayor por tiempo 
+--      de envío. 
+
+
+
+-- C) - Realizar un procedimiento almacenado que permita ingresar un plato. 
+
+
+
+-- D) - Realizar un procedimiento almacenado que permita conocer el total facturado de un día que 
+--      se envía como parámetro. 
+
+
+
+-- E) - Realizar un procedimiento almacenado que permita conocer el total de horas trabajadas por 
+--      un Chef en un mes. Se deberá ingresar el código de chef, el mes y año. 
+
+
+
+-- F) - Realizar un procedimiento almacenado que actualice el tiempo promedio de elaboración de 
+--      todos los platos, al valor calculado por el promedio de demora de los pedidos de cada plato en la 
+--      realidad. 
