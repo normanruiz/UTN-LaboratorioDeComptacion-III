ea023931d82358bb205ecd9be4f2a428216605fb
Norman Ruiz <norman.ruiz@outlook.com.ar>
2018-11-27 22:17:29 -0300

    Archivo SQL con las consignas para trabajar desde otra terminal

---
+++ TrabajoPracticoSugerido5/TPS_5_Cine_Norman_Ruiz.sql
@@ -0,0 +1,65 @@
+﻿/*
+Universisas Tecnologica Nacional
+General pacheco
+Laboratorio de Computacioon III
+Profesor: Angel Simon
+A�o 2018
+Segundo cuatrimestre
+Turno Noche
+Alumno Norman Ruiz
+Trabajo Practico Sugerido Nº5
+*/
+
+/*
+Consultas de selección y funciones de agregado 
+ 
+El siguiente ejercicio tiene como objetivo poner en práctica los conceptos de selección y el uso 
+de consultas que obtengan datos de diferentes tablas utilizando JOINS. Además se aplicará el 
+uso de funciones de agregado para resumir información. 
+ 
+Para ello se utilizará la base de datos creada en el Trabajo práctico sugerido N° 2 y se le 
+incorporarán los datos que figuran en el Trabajo práctico sugerido N° 3. 
+*/
+
+-- A) Hacer un listado que permita conocer el total recaudado agrupado por año. Se deberá mostrar 
+--    año y total recaudado. 
+
+
+-- B) Obtener el promedio de duración en minutos entre todas las películas. 
+
+
+-- C) Obtener el promedio de duración en minutos entre todas las películas que se hayan estrenado 
+--    desde el año 2000 en adelante. 
+
+
+-- D) Obtener el total recaudado agrupado por país. Se deberá mostrar el nombre del país y el total 
+--    recaudado. 
+
+
+-- E) Obtener la puntuación mínima entre todas las películas. 
+
+
+-- F) Obtener la cantidad de actores y actrices agrupados por nacionalidad. Se deberá mostrar el 
+--    nombre del país y la cantidad de actores. 
+
+
+-- G) Obtener el promedio de tickets vendidos de las películas estrenadas en el primer semestre y 
+--    en el segundo semestre. Se deberá obtener dos filas, la primera obteniendo el promedio de 
+--    tickets vendidos para el primer semestre y la segunda fila para el segundo. Sólo debe de tenerse 
+--    en cuenta los meses. Los días y años serán indiferentes. 
+
+
+-- H) Obtener la cantidad de películas realizadas por país. Si algún país no tiene relacionada ninguna 
+--    película, este deberá contabilizar cero. Se deberá mostrar nombre del país y cantidad de 
+--    películas. 
+
+
+-- I) Obtener el promedio de puntajes de películas agrupado por país. Incorporar al listado sólo 
+--    aquellos países que superen el valor promedio de 7,5. Se deberá mostrar nombre del país y 
+--    promedio de puntaje. 
+
+
+-- J) Obtener la fecha de nacimiento más cercana a la actual. 
+
+
+-- K) Obtener la cantidad de países en los que se haya realizado alguna película. 
